a8042b5729fd2056dfaa9c74e08be40c41a2dd4c
Thiago Arteweb <webdesignemfoco@gmail.com>
2017-10-16 10:45:53 -0200

    Alteração no public/index.php e Contrato.rtf

--- 01 - Base/public/index.php
+++ 01 - Base/public/index.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
-require_once(DIRREQUIRES."libs/vendor/autoload.php");
+require_once("../libs/vendor/autoload.php");
 
 use App\Init;
 $new = new Init;
