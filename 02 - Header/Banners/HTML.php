d01eb58acf4395ceda4219cfdc019dcafb198044
Thiago Arteweb <webdesignemfoco@gmail.com>
2017-10-16 11:44:58 -0200

    Upload Header/Banner

---
+++ 02 - Header/Banners/HTML.php
@@ -0,0 +1,9 @@
+<div class="banner">
+    <picture>
+        <source media="(max-width: 480px)" srcset="<?php echo DIRIMAGES . 'Banner-Contate-a-Almeida-e-Cruz-XS-01.jpg'; ?>">
+        <source media="(min-width: 481px) and (max-width: 768px)" srcset="<?php echo DIRIMAGES . 'Banner-Contate-a-Almeida-e-Cruz-S-01.jpg'; ?>">
+        <source media="(min-width: 769px) and (max-width: 1199px)" srcset="<?php echo DIRIMAGES . 'Banner-Contate-a-Almeida-e-Cruz-M-01.jpg'; ?>">
+        <source media="(min-width: 1200px)" srcset="<?php echo DIRIMAGES . 'Banner-Contate-a-Almeida-e-Cruz-L-01.jpg'; ?>">
+        <img src="<?php echo DIRIMAGES . 'Banner-Contate-a-Almeida-e-Cruz-S-01.jpg'; ?>" alt="Banner Sobre a L'única cortinas em Belo Horizonte" title="Banner Sobre a L'única cortinas em Belo Horizonte">
+    </picture>
+</div>
